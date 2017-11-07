70311f901070f55939071aaf5b0646cfe632f910
yolandajayson <yolanda.jayson@gmail.com>
2017-11-07 21:32:05 +0000

    Associated pins with users and added Authentication for users

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,58 +1,49 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
+def index
+  @pins = Pin.all
+end
+  
+def show
+end
 
-  # GET /pins
-  # GET /pins.json
-  def index
-    @pins = Pin.all
-  end
+def new
+  @pin = current_user.pins.build
+end
 
-  # GET /pins/1
-  # GET /pins/1.json
-  def show
-  end
+def edit
+end
 
-  # GET /pins/new
-  def new
-    @pin = Pin.new
-  end
-
-  # GET /pins/1/edit
-  def edit
-  end
-
-  # POST /pins
-  # POST /pins.json
-  def create
-    @pin = Pin.new(pin_params)
-
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render :show, status: :created, location: @pin }
-      else
-        format.html { render :new }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+def create
+  @pin = current_user.pins.build(pin_params)
+  respond_to do |format|
+    if @pin.save
+      format.html { redirect_to @pin, notice: 'Pin was successfully created.' } # --> (MM) You were missing the closing curly brace here.
+      format.json { render show, status: :created, location: @pin }    
+    else  # (MM) --> this was missing
+      format.html { render :new }
+      format.json { render json: @pin.errors, status: :unprocessable_entity }
     end
   end
-
-  # PATCH/PUT /pins/1
-  # PATCH/PUT /pins/1.json
-  def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pin }
+end
+def update
+  respond_to do |format|  # so was this
+    if @pin.update(pin_params)
+      # this line was missing: format.html and the curly braces
+      format.html { redirect_to @pin, notice: 'Pin was successfully updated' }
+      # Not sure what happened here, but the next line is the correct code:
+      format.json { render :show, status: :ok, location: @pin }
+      # And the following is what you had there:
+      # render action: "edit"
       else
         format.html { render :edit }
         format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
-    end
+    end # missing too
   end
-
-  # DELETE /pins/1
-  # DELETE /pins/1.json
+end # missing too
   def destroy
     @pin.destroy
     respond_to do |format|
@@ -60,15 +51,19 @@ class PinsController < ApplicationController
       format.json { head :no_content }
     end
   end
+# def correst_user  --> (MM) This was your original code; make sure to check your spelling! (this was a syntax error)
+# This wasn't what caused the error you were having, but it would have caused problems down the road.
+def correct_user
+  @pin=current_user.pins.find_by(id: params[:id])
+    redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pin
-      @pin = Pin.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
-      params.require(:pin).permit(:description)
-    end
+private
+  def set_pin
+    @pin = Pin.find(params[:id])
+  end
+  
+  def pin_params
+    params.require(:pin).permit(:description)
+  end
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,3 @@
 class Pin < ActiveRecord::Base
+    belongs_to :user    #   --> (MM) You had belongs_to :user_id here instead, causing a name error in the pins_controller.rb file
 end
